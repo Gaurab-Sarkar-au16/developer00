5fa4b9ee37479893d71c89595d55fb90607da48e
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2021-10-29 22:56:25 +0530

    29/10/21

--- backend/4.restAPI/node-rest-shop/api/routes/user.js
+++ backend/4.restAPI/node-rest-shop/api/routes/user.js
@@ -103,6 +103,7 @@ router.delete(":/userId", (req, res, next) => {
     })
     .catch((err) => {
       console.log(err);
+      console.log(err);
       res.status(500).json({
         error: err,
       });
