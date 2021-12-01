0f44dae1c921a54ab0e26dff457970a9c3b1eec6
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2021-12-01 22:41:06 +0530

    basic

--- reactjs/react-course/src/components/Todo.js
+++ reactjs/react-course/src/components/Todo.js
@@ -1,6 +1,16 @@
-function Todo(props) {
-  let deleteHandler = () => {
+import { useState } from 'react'
+import Modal from '../components/Modal'
+import Backdrop from '../components/Backdrop'
 
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  function deleteHandler() {
+    setModalIsOpen(true)
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false)
   }
   
   return (
@@ -9,6 +19,11 @@ function Todo(props) {
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+      {/* {modalIsOpen ? <Modal /> : null}      
+      {modalIsOpen ? <Backdrop /> : null} */}
+      {/* if both the values are true then second value is returned */}
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> }      
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler}/> }     
     </div>
   );
 }
