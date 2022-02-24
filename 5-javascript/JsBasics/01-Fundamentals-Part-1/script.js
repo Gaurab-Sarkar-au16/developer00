74aa5e9f419905f197f01bd1d89f3bf30cf34d07
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-02-24 21:50:09 +0530

    operators

--- 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
+++ 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
@@ -1,3 +1,60 @@
+// values and variables
+//**********************
 let js = "amazing";
-if ((js = "amazing")) alert("Javascript is FUN!");
-console.log(40+8+23-10);
+console.log(40 + 8 + 23 - 10);
+
+console.log("Gaurab");
+console.log(23);
+
+let firstName = "Vivek";
+console.log(firstName);
+
+let myFirstJob = "Quality Engineer";
+
+// data types 
+//****************
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+javascriptIsFun = "YES!";
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+
+console.log(typeof null);
+
+// let,const and var
+//*******************
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; //this will not work
+
+var height = 50;
+
+// basic operators
+//******************
+//Mathematical operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the power 3
+
+firstName = "Shraddha";
+const lastName = "Sil";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;// x = x + 1
+x--;// x = x - 1
+console.log(x);
+
+//Comparision operators
