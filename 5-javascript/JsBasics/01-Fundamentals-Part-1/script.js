47ad5b39fa3c0eeab564fa0bcc7fa31737de684a
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-07 09:50:54 +0530

    changes

--- 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
+++ 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
@@ -11,7 +11,7 @@ console.log(firstName);
 
 let myFirstJob = "Quality Engineer";
 
-// data types 
+// data types
 //****************
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -53,8 +53,74 @@ console.log(firstName + " " + lastName);
 let x = 10 + 5;
 x += 10; // x = x + 10 = 25
 x *= 4; // x = x * 4 = 100
-x++;// x = x + 1
-x--;// x = x - 1
+x++; // x = x + 1
+x--; // x = x - 1
 console.log(x);
 
 //Comparision operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+//Operator precedence
+//******************
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const avearageAge = ageJonas + ageSarah / 2;
+const avearageAgeCorrect = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avearageAge, avearageAgeCorrect);
+
+// Strings and template literals
+//*******************************
+
+const firstName = "Mike";
+const job = "mechanic";
+const birthYear = 1960;
+const year = 2022;
+
+const statement =
+  "I'm" + firstName + ", a" + (year - birthYear) + " years old " + job + "!";
+console.log(statement);
+
+const statementNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(statementNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String ith \n\
+multiple \n\
+lines");
+
+console.log(`String
+mmultiple 
+lines`);
+
+// if/else statement
+//******************
+const age = 19;
+
+if (age >= 18) {
+  console.log(`Sarah can start deriving license 🚗`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century)
+
+// Type Conversion and coersion
+//******************************
+
---
+++ 5-javascript/projects/AsynchronousJS(AsyncAwait_Promises_Callbacks)/index.js
@@ -0,0 +1,36 @@
+// // Synchronus
+// console.log(" I ")
+// console.log(" eat ")
+// console.log(" ice cream ")
+// console.log(" with a ")
+// console.log(" spoon ")
+
+// // Asynchronus
+// console.log(" I ");
+// console.log(" eat ");
+// setTimeout(() => {
+//   console.log(" ice cream ");
+// }, 4000);
+// console.log(" with a ");
+// console.log(" spoon ");
+
+// Callbacks
+// function one() {
+//  console.log("Step 1") 
+// } 
+// function two() {
+//  console.log("Step 2") 
+// } 
+// one()
+// two() 
+
+// function one(call_two) {
+//   console.log("step 1 complete. please call step 2")
+//   call_two() 
+//  } 
+// function two() {
+//   console.log("step 2") 
+//  } 
+// one(two)
+  
+
