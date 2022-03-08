50ddb52e9f19907106eee2bbdb401465ece0ad21
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-08 12:10:18 +0530

    boolean-logical operators, switch and conditional statements

--- 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
+++ 5-javascript/JsBasics/01-Fundamentals-Part-1/script.js
@@ -124,78 +124,170 @@
 // Type Conversion and coersion
 //******************************
 // type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(inputYear + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(inputYear + 18);
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// type coersion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" * "2");
-console.log("23" / "2");
+// // type coersion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" * "2");
+// console.log("23" / "2");
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
 
-console.log(2 + 3 + 4 + "5");
-console.log("10" - "4" - "3" - 2 + "5");
+// console.log(2 + 3 + 4 + "5");
+// console.log("10" - "4" - "3" - 2 + "5");
 
 // Truthy and Falsy Values
 //******************************
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Jonas"));
-console.log(Boolean({}));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
 
-const money=0;
-if(money){
-  console.log("Dont spend it all ;)")
-} else {
-  console.log('You should get a job')
-}
+// const money=0;
+// if(money){
+//   console.log("Dont spend it all ;)")
+// } else {
+//   console.log('You should get a job')
+// }
 
-let height;
-if(height) {
-  console.log('YAY! height is defined')
-} else {
-  console.log('Height is UNDEFINED')
-}
+// let height;
+// if(height) {
+//   console.log('YAY! height is defined')
+// } else {
+//   console.log('Height is UNDEFINED')
+// }
 
 // Equality Operators: == vs. ===
 //*********************************
 
-let age = 18;
-if (age === 18) console.log('You just bacame an adult :D')
+// let age = 18;
+// if (age === 18) console.log('You just bacame an adult :D')
 
-age = '18';
-if (age === 18) console.log('You just bacame an adult :D')
+// age = '18';
+// if (age === 18) console.log('You just bacame an adult :D')
 
-const favourite = Number(prompt("What's your favourite number?"))
-console.log(favourite)
-console.log(typeof favourite)
+// const favourite = Number(prompt("What's your favourite number?"))
+// console.log(favourite)
+// console.log(typeof favourite)
 
-if(favourite === 23) {
-  console.log('Cool! 23 is an amazing number')
-} else if(favourite === 7) {
-  console.log('7 is also a cool number')
-} else if(favourite===9){
-  console.log('9 is also a cool number')
-} else {
-  console.log('Number is not 23 or 7 or 9')
-}
+// if(favourite === 23) {
+//   console.log('Cool! 23 is an amazing number')
+// } else if(favourite === 7) {
+//   console.log('7 is also a cool number')
+// } else if(favourite===9){
+//   console.log('9 is also a cool number')
+// } else {
+//   console.log('Number is not 23 or 7 or 9')
+// }
 
-if(favourite !== 23) console.log('Why not 23?')
-
+// if(favourite !== 23) console.log('Why not 23?')
 
 // Boolean logic:
 //*********************************
 
+// Logical Operators:
+//*********************************
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
 
+// console.log(hasDriversLicense && hasGoodVision); //and operator
+// console.log(hasDriversLicense || hasGoodVision); //and operator
+// console.log(!hasDriversLicense); //not operator
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+// const isTired = false; //C
+// console.log(hasDriversLicense && hasGoodVision && isTired)
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+// The switch statement:
+//*********************************
+
+// const day = "monday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Record videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Enjoy the weekend :D");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// The Conditional(ternary) Operator:
+//*************************************
+
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+
+let drink2
+if (age>=18) {
+  drink2 = 'wine 🍷'
+} else {
+  drink2 = 'water 💧'
+}
+console.log(drink2)
+
+console.log(`I like to drink ${age>=18 ? "wine 🍷" : "water 💧"}`)
