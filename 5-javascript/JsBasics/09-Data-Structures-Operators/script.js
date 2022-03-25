4c4b74c0f7237397ad7077f531225f3073ed56e5
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-25 15:06:39 +0530

    destructuring objrcts

--- 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
+++ 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
@@ -31,7 +31,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  orderDelivery: function ({ starterIndex=1, mainIndex=0, time='20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
@@ -44,6 +44,11 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 2,
 });
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+})
 
 // const arr = [2, 3, 4];
 // const a = arr[0];
