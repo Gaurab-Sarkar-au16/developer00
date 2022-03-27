6f918658d7e6922b9d705b7ca0c1c5f93e3a2d0f
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-27 23:48:09 +0530

    short circuiting

--- 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
+++ 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
@@ -48,11 +48,41 @@ const restaurant = {
     );
   },
 
-  orderPizza: function(mainIngredient,...otherIngredients){
-    console.log(mainIngredient)
-    console.log(otherIngredients)
-  }
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+// Short ircuiting(&& and ||):
+// ******************************
+
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 // restaurant.orderDelivery({
 //   time: '22:30',
@@ -176,41 +206,43 @@ const restaurant = {
 // console.log(restaurantCopy.name)
 // console.log(restaurant.name)
 
+// Rest pattern and parameter:
+// ******************************
 // 1) Destructuring
 
 // SPREAD, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
+// const arr = [1, 2, ...[3, 4]];
 
-// REST, because on LEFT side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+// // REST, because on LEFT side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-const [pizza, risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+// const [pizza, risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
-// Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+// // Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-// 2) Functions
-const add = function (...numbers) {
-  let sum =0
-  for (let i=0; i<numbers.length;i++){
-    sum += numbers[i]
-  }
-  console.log(sum)
-  // console.log(numbers);
-};
+// // 2) Functions
+// const add = function (...numbers) {
+//   let sum =0
+//   for (let i=0; i<numbers.length;i++){
+//     sum += numbers[i]
+//   }
+//   console.log(sum)
+//   // console.log(numbers);
+// };
 
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
 
-const x = [23,5,7]
-add(...x)
+// const x = [23,5,7]
+// add(...x)
 
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
-restaurant.orderPizza('mushrooms')
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
+// restaurant.orderPizza('mushrooms')
