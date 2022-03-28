86ace518dfe6f2997fe1070f0fdf2d3321a646d3
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-28 12:03:59 +0530

    nullish operator

--- 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
+++ 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
@@ -54,35 +54,50 @@ const restaurant = {
   },
 };
 
+// Logical Assignment Operators:
+// *****************************************
+
+
+// The Nullish Coalescing Operator (??):
+// *****************************************
+// restaurant.numGuests = 0
+// const guests = restaurant.numGuests || 10
+// console.log(guests)
+
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10
+// console.log(guestCorrect)
+
+
 // Short ircuiting(&& and ||):
 // ******************************
 
 // Use ANY data type, return ANY data type, short-circuiting
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
 
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-// restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// // restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
 
-console.log('---- AND ----');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
+// console.log('---- AND ----');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
 
-console.log('Hello' && 23 && null && 'jonas');
+// console.log('Hello' && 23 && null && 'jonas');
 
-// Practical example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// // Practical example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 // restaurant.orderDelivery({
 //   time: '22:30',
