19bf346774782a4d7c7f2faa8d3210151df2c933
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-03-28 16:10:05 +0530

    logical assignment operators

--- 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
+++ 5-javascript/JsBasics/09-Data-Structures-Operators/script.js
@@ -56,7 +56,34 @@ const restaurant = {
 
 // Logical Assignment Operators:
 // *****************************************
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
 
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// nullish assignment operator(null or undefined)
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
 
 // The Nullish Coalescing Operator (??):
 // *****************************************
@@ -67,7 +94,6 @@ const restaurant = {
 // // Nullish: null and undefined (NOT 0 or '')
 // const guestCorrect = restaurant.numGuests ?? 10
 // console.log(guestCorrect)
-
 
 // Short ircuiting(&& and ||):
 // ******************************
