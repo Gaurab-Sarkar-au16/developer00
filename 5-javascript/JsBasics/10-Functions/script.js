4d16f9e2b3ff1ecf505397bae11f0d87cbe7da1f
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-04-06 23:45:59 +0530

    the call and apply methods

--- 5-javascript/JsBasics/10-Functions/script.js
+++ 5-javascript/JsBasics/10-Functions/script.js
@@ -122,3 +122,49 @@
 
 // The call and apply Methods
 // *****************************************
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+  },
+};
+
+lufthansa.book(239, 'Vivek Bindra');
+lufthansa.book(635, 'Jhon Smith');
+console.log(lufthansa)
+
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book
+
+// Does NOT work
+// book(23,'Sarah Williams')
+
+book.call(eurowings, 23, 'Sarah Williams')
+console.log(eurowings)
+
+book.call(lufthansa, 239, 'Mary Cooper')
+console.log(lufthansa)
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: []
+}
+
+book.call(swiss, 583, 'Mary Cooper')
+console.log(swiss)
+
+// Apply method
+const flightData = []
