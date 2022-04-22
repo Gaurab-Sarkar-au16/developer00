6b71493c3be09fdab6c983143b5db5098261b05c
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-04-22 16:43:39 +0530

    computing usernames

--- 5-javascript/JsBasics/11-Arrays-Bankist/script.js
+++ 5-javascript/JsBasics/11-Arrays-Bankist/script.js
@@ -83,6 +83,22 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+// Computing Usernames
+// ***************************
+// const user = 'Steven Thomas Williams';
+//stw
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+
+};
+createUsernames(accounts);
+console.log(accounts)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -210,22 +226,22 @@ displayMovements(account1.movements);
 // The map Method
 // **********************
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const eurToUsd = 1.1;
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
 
 // const movementsUSD = movements.map(function (mov) {
 //   return mov * eurToUsd;
 // });
 
 // arrow function
-const movementsUSD = movements.map(mov => mov * eurToUsd);
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
 
-console.log(movements);
-console.log(movementsUSD);
+// console.log(movements);
+// console.log(movementsUSD);
 
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
 
 // const movementsDescriptions = movements.map((mov,i,arr)=>{
 // if(mov>0){
@@ -235,11 +251,11 @@ console.log(movementsUSDfor);
 // }
 // })
 
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
-      mov
-    )}`
-);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
 
-console.log(movementsDescriptions);
+// console.log(movementsDescriptions);
