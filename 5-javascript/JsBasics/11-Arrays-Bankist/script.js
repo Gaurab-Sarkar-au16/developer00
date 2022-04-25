7d5ac4ddeadc2cf670eb0afcdde58a00c5ebb64a
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-04-25 12:18:21 +0530

    implementing transfers

--- 5-javascript/JsBasics/11-Arrays-Bankist/script.js
+++ 5-javascript/JsBasics/11-Arrays-Bankist/script.js
@@ -83,9 +83,9 @@ const displayMovements = function (movements) {
 };
 
 // Displaying the sum total of movements:
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -131,6 +131,17 @@ console.log(accounts);
 // Implementing Login
 // ***************************
 
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
 // Event handler
 let currentAccount;
 
@@ -152,17 +163,39 @@ btnLogin.addEventListener('click', function (e) {
 
     // Clear input field
     inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur()
+    inputLoginPin.blur();
 
-    // Display movements
-    displayMovements(currentAccount.movements);
-
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
-
-    // Display summary
-    calcDisplaySummary(currentAccount);
+    // Update UI
+    updateUI(currentAccount);
   }
+});
+
+// // Implementing Transfers
+// ***************************
+btnTransfer.addEventListener('click', function (e) {
+  // prevents the form from reloading
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  // console.log(amount, receiverAcc);
+  inputTransferAmount.value = inputTransferTo.value = ''
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    // console.log(`Transfer valid`)
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }  
 });
 
 /////////////////////////////////////////////////
