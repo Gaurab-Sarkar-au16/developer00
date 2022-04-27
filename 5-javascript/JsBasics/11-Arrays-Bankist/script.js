56d99364b8b5cb48ca70fcf800dd7f39a75e6130
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-04-27 12:16:34 +0530

    array practice

--- 5-javascript/JsBasics/11-Arrays-Bankist/script.js
+++ 5-javascript/JsBasics/11-Arrays-Bankist/script.js
@@ -564,48 +564,106 @@ btnSort.addEventListener('click', function (e) {
 // More Ways of Creating and Filling Arrays
 // ***************************************************
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-const x = new Array(7);
-console.log(x);
-// console.log(x.map(()=>5))
+// const x = new Array(7);
+// console.log(x);
+// // console.log(x.map(()=>5))
 
-// x.fill(1)
-x.fill(1, 3, 5);
-x.fill(1);
-console.log(x);
+// // x.fill(1)
+// x.fill(1, 3, 5);
+// x.fill(1);
+// console.log(x);
 
-arr.fill(23, 2, 6);
-console.log(arr);
+// arr.fill(23, 2, 6);
+// console.log(arr);
 
-// Array.from
-const y = Array.from({ length: 7 }, () => 1);
-console.log(y);
+// // Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
 
-const z = Array.from({ length: 7 }, (_, i) => i + 1);
-console.log(z);
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
 
-const randomDiceRolls = Array.from({ length: 100 }, () =>
-  Math.round(Math.random() * 6)
-);
-console.log(randomDiceRolls);
+// const randomDiceRolls = Array.from({ length: 100 }, () =>
+//   Math.round(Math.random() * 6)
+// );
+// console.log(randomDiceRolls);
+
+// // labelBalance.addEventListener('click', function () {
+// //   const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+
+// //   console.log(movementsUI.map(el => Number(el.textContent.replace('€', ''))));
+// // });
 
 // labelBalance.addEventListener('click', function () {
-//   const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+//   const movementsUI = Array.from(
+//     document.querySelectorAll('.movements__value'),
+//     el => Number(el.textContent.replace('€', ''))
+//   );
+//   console.log(movementsUI);
 
-//   console.log(movementsUI.map(el => Number(el.textContent.replace('€', ''))));
+//   const movementsUI2 = [...document.querySelectorAll('.movements__value')]
+//   console.log(movementsUI2)
 // });
 
-labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('€', ''))
+// Array Methods Practice
+// ******************************
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, curr) => sum + curr, 0);
+console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+// console.log(numDeposits1000);
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+
+// Prefixed ++ operator
+let a = 10;
+console.log(++a);
+console.log(a);
+
+// 3.
+const sums = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, curr) => {
+      // cur > 0 ? (sums.deposits += curr) : (sums.withdrawals += curr);
+      sums[curr > 0 ? 'deposits' : 'withdrawals'] += curr;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
   );
-  console.log(movementsUI);
+console.log(sums);
 
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')]
-  console.log(movementsUI2)
-});
+// 4.
+// this is a nice title -> This Is a Nice Title
 
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1)
 
+  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => exceptions.includes(word) ? word: capitalize(word))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
