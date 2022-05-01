9c4bf836f92cb295c17f70ac0f226d63fc9e681d
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-05-01 23:49:53 +0530

    numeric seperators

--- 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
+++ 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
@@ -358,3 +358,20 @@ btnSort.addEventListener('click', function (e) {
 //     if (i % 2 === 0) row.style.backgroundColor = 'orangered';
 //   });
 // });
+
+// Numeric Separators
+// ******************************
+
+// 287,460,000,000
+const diameter = 287_460_000_000
+console.log(diameter)
+
+const price= 345_99
+console.log(price)
+
+const transferFee1 = 15_00 
+const transferFee2 = 1_500
+
+const PI = 3.1415
+console.log(PI)
+
