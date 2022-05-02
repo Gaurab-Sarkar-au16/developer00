0619f6efec7ce2aba19b36ba914d1b6b83c9e2c3
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-05-02 18:38:27 +0530

    Big Int

--- 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
+++ 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
@@ -362,16 +362,48 @@ btnSort.addEventListener('click', function (e) {
 // Numeric Separators
 // ******************************
 
-// 287,460,000,000
-const diameter = 287_460_000_000
-console.log(diameter)
+// // 287,460,000,000
+// const diameter = 287_460_000_000
+// console.log(diameter)
 
-const price= 345_99
-console.log(price)
+// const price= 345_99
+// console.log(price)
 
-const transferFee1 = 15_00 
-const transferFee2 = 1_500
+// const transferFee1 = 15_00 
+// const transferFee2 = 1_500
 
-const PI = 3.1415
-console.log(PI)
+// const PI = 3.1415
+// console.log(PI)
+
+// console.log(Number('230_000'))
+// console.log(parseInt('230_000'))
+
+// Working with BigInt
+// **************************
+
+console.log(48354582465621202115641616846n)
+console.log(BigInt(4851561122312))
+
+// Operations
+console.log(10000n + 10000n)
+console.log(646464821645484548654856478123n + 100000000n)
+console.log(huge*BigInt(num))
+
+const huge = 48454865485647812322984353n
+const num = 23
+// console.log(huge + num)
+console.log(huge + BigInt(num))
+
+// Exceptions
+console.log(20n > 15)
+console.log(20n === 20)
+console.log(typeof 20n)
+console.log(20n === '20')
+
+console.log(huge + ' is really big')
+
+// Divisions 
+console.log(10n/3n)
+console.log(10/3)
+
 
