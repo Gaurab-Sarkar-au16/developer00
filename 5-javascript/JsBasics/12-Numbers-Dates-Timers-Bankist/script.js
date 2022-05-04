78040a77f85e594a19cdde5b7d072378b6da3c1a
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-05-04 16:08:33 +0530

    setTimer and setInterval

--- 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
+++ 5-javascript/JsBasics/12-Numbers-Dates-Timers-Bankist/script.js
@@ -144,7 +144,6 @@ const calcDisplayBalance = function (acc) {
 };
 
 const calcDisplaySummary = function (acc) {
-  
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
@@ -280,14 +279,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -555,4 +556,24 @@ btnSort.addEventListener('click', function (e) {
 // Timers: setTimeout and setInterval
 // *****************************************
 
+// setTImeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza 🍕 with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
 
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+// setInterval(function(){
+//   const now = new Date()
+//   console.log(now)
+// }, 1000)
+
+setInterval(function(){
+  const now = new Date()
+  console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`)
+}, 1000)
