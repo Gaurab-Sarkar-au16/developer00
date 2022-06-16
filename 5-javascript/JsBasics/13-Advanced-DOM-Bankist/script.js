7e2c00aaabccf0860fa1b88503ae3b078e46c21f
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-06-16 12:44:43 +0530

    sticky navigation

--- 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
+++ 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
@@ -143,6 +143,20 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Implementing a Sticky Navigation: The Scroll Event
+// **********************************************************
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect()
+console.log(initialCoords)
+
+window.addEventListener('scroll', function(){
+  console.log(window.scrollY)
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+})
+
 // Selecting, Creating, and Deleting Elements
 // ***************************************************
 
