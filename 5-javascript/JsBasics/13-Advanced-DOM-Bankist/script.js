8f9cbd2eabe759ab22d22db1537d17a9c4782d05
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-06-19 16:57:23 +0530

    Lazy Loading Images

--- 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
+++ 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
@@ -226,15 +226,22 @@ const loadImg = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
 
-  if(!entry.isIntersecting) return
+  if (!entry.isIntersecting) return;
 
   // Replace src with data-src
-  
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img')
+  });
+
+  observer.unobserve(entry.target)
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '200px'
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
