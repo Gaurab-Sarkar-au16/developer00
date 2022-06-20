630c545efedebfec6f2bb0f01b38b48a344053fd
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2022-06-20 23:45:03 +0530

    changes

--- 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
+++ 5-javascript/JsBasics/13-Advanced-DOM-Bankist/script.js
@@ -215,7 +215,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // Lazy Loading Images
@@ -232,19 +232,31 @@ const loadImg = function (entries, observer) {
   entry.target.src = entry.target.dataset.src;
 
   entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img')
+    entry.target.classList.remove('lazy-img');
   });
 
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
-  rootMargin: '200px'
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Building a Slider Component: Part 1
+// ******************************************
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
 
 // Selecting, Creating, and Deleting Elements
 // ***************************************************
