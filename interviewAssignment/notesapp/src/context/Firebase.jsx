06a935bd1a644820b26f5a841a309aacb3d7b5dc
Gaurab Lenovo <gaurab2193@gmail.com>
2023-03-15 18:59:10 +0530

    changes

--- interviewAssignment/notesapp/src/context/Firebase.jsx
+++ interviewAssignment/notesapp/src/context/Firebase.jsx
@@ -26,12 +26,6 @@ const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
   //Enter firebase config
-  apiKey: "AIzaSyDsfxDCitDE-jolSdoECRqi6swHKcQsIhY",
-  authDomain: "bookify-54746.firebaseapp.com",
-  projectId: "bookify-54746",
-  storageBucket: "bookify-54746.appspot.com",
-  messagingSenderId: "1082765797863",
-  appId: "1:1082765797863:web:789cebe4a518fb563fb542"
 };
 
 //custom hook
