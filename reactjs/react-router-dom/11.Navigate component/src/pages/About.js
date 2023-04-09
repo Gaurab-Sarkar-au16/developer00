c60d8be05b7bceeccf9f214dee78790b80fdac64
Gaurab Lenovo <gaurab2193@gmail.com>
2023-04-10 00:03:55 +0530

    complete

--- reactjs/react-router-dom/11.Navigate component/src/pages/About.js
+++ reactjs/react-router-dom/11.Navigate component/src/pages/About.js
@@ -1,8 +1,11 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 export default function About() {
   const [user, setUser] = useState('mario')
 
   if(!user){
-    return
+    return <Navigate to="/" replace={true}/>
   }
     return (
       <div className="about">
