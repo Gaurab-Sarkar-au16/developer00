4ad1e4fae85164fc4a197489feda9654a704ccc3
Gaurab Lenovo <gaurab2193@gmail.com>
2023-04-14 23:50:20 +0530

    useReducer

--- reactjs/hooks/usereducer/src/App.js
+++ reactjs/hooks/usereducer/src/App.js
@@ -1,24 +1,54 @@
-import { useState } from 'react';
-import './App.css';
+import { useReducer, useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
+
+const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo"
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map
+  }
+}
+
+function newTodo(name) {
+  return {
+    id: Date.now(),
+    name: name,
+    complete: false,
+  };
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
 
-  function increment(){
-    setCount(prevCount => prevCount + 1)
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
   }
 
-  function decrement(){
-    setCount(prevCount => prevCount - 1)
-  }
+  console.log(todos);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>useReducer</h1>
-        <button onClick={decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={increment}>+</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            onchange={(e) => setName(e.target.value)}
+          />
+        </form>
+        {todos.map(todo => {
+          return <Todo key={todo.id} todo={todo}/>
+        })}
       </header>
     </div>
   );
