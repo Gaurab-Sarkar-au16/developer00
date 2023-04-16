1de5ad342badee2ae39de03c597dd4eae7ff0ff9
Gaurab Lenovo <gaurab2193@gmail.com>
2023-04-16 15:02:53 +0530

    useReducer

--- reactjs/hooks/usereducer/src/Todo.js
+++ reactjs/hooks/usereducer/src/Todo.js
@@ -1,13 +1,27 @@
-function Todo({todo}) {
-    return(
-        <div>
-            <span style={{color: todo.complete?'#AAA':'#000'}}>
-                {todo.name}
-            </span>
-            <button>Toggle</button>
-            <button>Delete</button>
-        </div>
-    )
+import { ACTIONS } from "./App";
+
+function Todo({ todo, dispatch }) {
+  return (
+    <div>
+      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })
+        }
+      >
+        Toggle
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
+        }
+      >
+        Delete
+      </button>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
