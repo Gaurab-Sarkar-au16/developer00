cbe2e19c4cdac13f73d6adc2ce237f092e047b71
Gaurab Lenovo <gaurab2193@gmail.com>
2023-05-30 16:05:39 +0530

    restAPI

--- interviewAssignment/revision/PROJECTS/2_shop_rest_api/app.js
+++ interviewAssignment/revision/PROJECTS/2_shop_rest_api/app.js
@@ -1,10 +1,47 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose")
 
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(`mongodb+srv://gaurab2108:password21@cluster0.szxzs.mongodb.net/?retryWrites=true&w=majority`)
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// CORS error
 app.use((req,res,next)=>{
-    res.status(200).json({
-        message:'It works! '
-    })
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
 })
 
-module.exports = app
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
