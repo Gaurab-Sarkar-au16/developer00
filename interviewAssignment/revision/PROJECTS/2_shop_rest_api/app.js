a313005b7883569af18009acb1a24d03df62aad2
Gaurab Lenovo <gaurab2193@gmail.com>
2023-06-03 13:33:46 +0530

    user signup

--- interviewAssignment/revision/PROJECTS/2_shop_rest_api/app.js
+++ interviewAssignment/revision/PROJECTS/2_shop_rest_api/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user")
 
 mongoose.connect(`mongodb+srv://gaurab2108:password21@cluster0.szxzs.mongodb.net/?retryWrites=true&w=majority`)
 
@@ -21,7 +22,7 @@ app.use((req,res,next)=>{
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     )
     if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET') 
         return res.status(200).json({})
     }
     next()
@@ -30,6 +31,7 @@ app.use((req,res,next)=>{
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes)
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
