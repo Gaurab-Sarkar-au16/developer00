2d92fd908f82c30b90a9b999f1a9df7521fe0899
Gaurab Lenovo <gaurab2193@gmail.com>
2023-06-09 23:41:09 +0530

    css

--- reactjs/react18Freecodecamp/tutorial/src/index.js
+++ reactjs/react18Freecodecamp/tutorial/src/index.js
@@ -1,41 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.css";
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />      
-      <Book />      
-      <Book />      
+      <Book />
+      <Book />
+      <Book />
     </section>
   );
 }
 
 const Book = () => {
+  const author = "James Clear"
+  const title = "Atomic Habits"
   return (
+
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/bookImage1.jpg" alt="Atomic Habits" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <p></p>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg"
-    alt="Atomic Habits"
-  />
-);
-const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => {
-  return <h4>James Clear</h4>;
-};
-
-const Message = () => {
-  return <p>this is my message</p>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
