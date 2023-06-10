2e2601061bc91d6cc2b7a3143ead17728724f2e5
Gaurab Lenovo <gaurab2193@gmail.com>
2023-06-10 22:54:35 +0530

    event

--- reactjs/react18Freecodecamp/tutorial/src/index.js
+++ reactjs/react18Freecodecamp/tutorial/src/index.js
@@ -3,26 +3,42 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "./images/bookImage1.jpg",
+    id: 1,
+  },
+  {
+    author: "Morgan Housel",
+    title: "The Psychology Of Money",
+    img: "./images/bookImage2.jpg",
+    id: 2,
+  },
+];
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        // console.log(book)
+        // const { img, title, author, id } = book;
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
 
-const Book = () => {
-  const author = "James Clear"
-  const title = "Atomic Habits"
+const Book = (props) => {
+  // console.log(props);
+  const {img, title, author} = props
   return (
-
     <article className="book">
-      <img src="./images/bookImage1.jpg" alt="Atomic Habits" />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <p></p>
+      {/* {children} */}
     </article>
   );
 };
