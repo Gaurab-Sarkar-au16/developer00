4e1022e18f5e5912bafdf63520a8fae0c929ffc9
Gaurab Lenovo <gaurab2193@gmail.com>
2023-06-26 23:37:56 +0530

    react fundamentals

--- reactjs/react18Freecodecamp/tutorial/src/index.js
+++ reactjs/react18Freecodecamp/tutorial/src/index.js
@@ -3,45 +3,23 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const books = [
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "./images/bookImage1.jpg",
-    id: 1,
-  },
-  {
-    author: "Morgan Housel",
-    title: "The Psychology Of Money",
-    img: "./images/bookImage2.jpg",
-    id: 2,
-  },
-];
+import books from "./books";
+import Book from "./Book";
 
 function Booklist() {
   return (
+    <>
+    <h1>amazon best sellers</h1>
     <section className="booklist">
-      {books.map((book) => {
+      {books.map((book, index) => {
         // console.log(book)
         // const { img, title, author, id } = book;
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} number={index}/>;
       })}
     </section>
+    </>
   );
 }
-
-const Book = (props) => {
-  // console.log(props);
-  const {img, title, author} = props
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      {/* {children} */}
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Booklist />);
