75de5183a340b008dbb5c3d87fc606d01a485938
Gaurab Lenovo <gaurab2193@gmail.com>
2023-07-06 22:37:53 +0530

    useReducer

--- reactjs/react18Freecodecamp/2_my-react-app_advanced/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ reactjs/react18Freecodecamp/2_my-react-app_advanced/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,42 +1,48 @@
-import {useState, useReducer} from "react";
+import { useReducer } from "react";
 import { data } from "../../../data";
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from './action';
+import reducer from './reducer';
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
 
 const ReducerBasics = () => {
-  const [people, setPeople] = useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
   };
+
   const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
     // setPeople([])
-  }
+  };
+
   const resetList = () => {
+    dispatch({ type: RESET_LIST });
     // setPeople(data);
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
-            <button onClick={() => removeItem(id)} className="btn">remove</button>
+            <button onClick={() => removeItem(id)} className="btn">
+              remove
+            </button>
           </div>
         );
       })}
-      {/* <button
-        className="btn"
-        style={{ marginTop: "2rem" }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
-      <br /> */}
-      {people.length > 0 ? (
+      {state.people.length > 0 ? (
         <button
           className="btn"
-          style={{ marginTop: "2rem" , background:'red'}}
+          style={{ marginTop: "2rem", background: "red" }}
           onClick={clearList}
         >
           remove all
@@ -44,7 +50,7 @@ const ReducerBasics = () => {
       ) : (
         <button
           className="btn"
-          style={{ marginTop: "2rem", background:'green' }}
+          style={{ marginTop: "2rem", background: "green" }}
           onClick={resetList}
         >
           reset items
--- reactjs/react18Freecodecamp/2_my-react-app_advanced/src/tutorial/10-useReducer/starter/reducer.js
+++ reactjs/react18Freecodecamp/2_my-react-app_advanced/src/tutorial/10-useReducer/starter/reducer.js
@@ -1,21 +1,24 @@
-import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from './actions';
-import { data } from '../../../data';
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./action";
+
+import { data } from "../../../data";
 
 const reducer = (state, action) => {
   if (action.type === CLEAR_LIST) {
     return { ...state, people: [] };
   }
+
   if (action.type === RESET_LIST) {
     return { ...state, people: data };
   }
+
   if (action.type === REMOVE_ITEM) {
     let newPeople = state.people.filter(
       (person) => person.id !== action.payload.id
     );
-
     return { ...state, people: newPeople };
   }
-  // return state;
+
+  // return state
   throw new Error(`No matching "${action.type}" - action type`);
 };
 
